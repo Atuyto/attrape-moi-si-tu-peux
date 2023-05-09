e5c614709bc049a1875623b3b2597b29590ce794
Softy7 <ewanrecquignies@gmail.com>
2023-05-09 10:29:25 +0200

    Ajout d'une case

--- attrape_moi_si_tu_peux/src/main/java/Application.java
+++ attrape_moi_si_tu_peux/src/main/java/Application.java
@@ -3,6 +3,7 @@ public class Application {
     public static void main(String[] arg)
     {
         Labyrinthe l1 = new Labyrinthe(10,10);
+        Case case = new Case;
 
         l1.genererGrille();
     }
