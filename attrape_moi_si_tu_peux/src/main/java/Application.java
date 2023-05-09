98ca70b6fec6af64c90f89b8516739260ba4a0cf
BigLyeyyy <BigLeyy.hannecart@gmail.com>
2023-05-09 10:51:42 +0200

    j'ajoute case

--- attrape_moi_si_tu_peux/src/main/java/Application.java
+++ attrape_moi_si_tu_peux/src/main/java/Application.java
@@ -2,8 +2,8 @@ public class Application {
 
     public static void main(String[] arg)
     {
-        Labyrinthe l1 = new Labyrinthe(10,10).
-
+        Labyrinthe l1 = new Labyrinthe(10,10);
+        Case c;
         l1.genererGrille();
     }
 }
