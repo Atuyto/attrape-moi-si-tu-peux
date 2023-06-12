af08a94747ceb8f0f93f443a96e03566d36c21a6
Atuyto <111154530+Atuyto@users.noreply.github.com>
2023-06-12 19:54:35 +0200

    test

--- attrape_moi_si_tu_peux/src/main/java/com/example/attrape_moi_si_tu_peux/Model/Dijkstra.java
+++ attrape_moi_si_tu_peux/src/main/java/com/example/attrape_moi_si_tu_peux/Model/Dijkstra.java
@@ -1,19 +1,16 @@
 package com.example.attrape_moi_si_tu_peux.Model;
 
 import java.util.*;
-import java.util.PriorityQueue;
 
 public class Dijkstra {
 
     private Labyrinthe lab;
-    private List<List<List<Integer>>> listeCoordonnees;
     private Case depart;
 
     private Case sortie;
 
     public Dijkstra(Labyrinthe labyrinthe, Case depart) {
         this.lab = labyrinthe;
-        this.listeCoordonnees = new ArrayList<>();
         this.depart = depart;
     }
     public int[] getSortie(){
@@ -31,52 +28,40 @@ public class Dijkstra {
         return new int[]{x,y};
     }
 
-    public List<List<Integer>> voisins(List<List<List<Integer>>> liste) {
-        List<List<Integer>> listVoisinsExplorer = new ArrayList<>();
-        List<List<Integer>> listVoisins = new ArrayList<>();
-        for (int i = 0; i < lab.getX(); i++) {
-            for (int j = 0; j < lab.getY(); j++) {
-                for (int k = 0; k < listVoisins.size(); k++) {
-                    if (!(listVoisinsExplorer.contains(listVoisins.get(k)))) {
-                        if (lab.getLesCases()[liste.get(i).get(j).get(0) - 1][liste.get(i).get(j).get(1)].isAccessible() && i < lab.getX() && j != 0) {
-                            List<Integer> l = new ArrayList<>();
-                            l.add(liste.get(i).get(j).get(0) - 1);
-                            l.add(liste.get(i).get(j).get(1));
-                            listVoisins.add(l);
-                            listVoisinsExplorer.add(l);
-                        }
-                        if (lab.getLesCases()[liste.get(i).get(j).get(0) + 1][liste.get(i).get(j).get(1)].isAccessible() && i < lab.getX() - 1 && j < lab.getY() - 1) {
-                            List<Integer> l = new ArrayList<>();
-                            l.add(liste.get(i).get(j).get(0) + 1);
-                            l.add(liste.get(i).get(j).get(1));
-                            listVoisins.add(l);
-                            listVoisinsExplorer.add(l);
-                        }
-                        if (lab.getLesCases()[liste.get(i).get(j).get(0)][liste.get(i).get(j).get(1) - 1].isAccessible() && j != 0 && i < lab.getX() - 1) {
-                            List<Integer> l = new ArrayList<>();
-                            l.add(liste.get(i).get(j).get(0));
-                            l.add(liste.get(i).get(j).get(1) - 1);
-                            listVoisins.add(l);
-                            listVoisinsExplorer.add(l);
-                        }
-                        if (lab.getLesCases()[liste.get(i).get(j).get(0)][liste.get(i).get(j).get(1) + 1].isAccessible() && j < lab.getY() - 1 && i != 0) {
-                            List<Integer> l = new ArrayList<>();
-                            l.add(liste.get(i).get(j).get(0));
-                            l.add(liste.get(i).get(j).get(1) + 1);
-                            listVoisins.add(l);
-                            listVoisinsExplorer.add(l);
-                        }
-                    }
+    public int[][] voisins(int[][] tableauCoordonnees, int[] point) {
+        int[][] voisinsExplorer = new int[lab.getX()][lab.getY()];
+        int[][] voisins = new int[lab.getX() * lab.getY()][2];
+        int count = 0;
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        int x = point[0];
+        int y = point[1];
+
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+
+            if (nextX >= 0 && nextX < lab.getX() && nextY >= 0 && nextY < lab.getY()) {
+                if (lab.getLesCases()[nextX][nextY].isAccessible() && voisinsExplorer[nextX][nextY] == 0) {
+                    voisins[count][0] = nextX;
+                    voisins[count][1] = nextY;
+                    voisinsExplorer[nextX][nextY] = 1;
+                    count++;
                 }
             }
         }
-        System.out.println(listVoisinsExplorer);
-        System.out.println(listVoisins);
-        return listVoisins;
+
+        // Réduire la taille du tableau de voisins pour éliminer les cases inutilisées
+        int[][] trimmedVoisins = new int[count][2];
+        System.arraycopy(voisins, 0, trimmedVoisins, 0, count);
+
+        return trimmedVoisins;
     }
 
 
-    public int[][] setWeight(){
+    public int[][] initPoids(){
         int[][] poidLab;
         poidLab = new int[lab.getX()][lab.getY()];
         for(int i = 0; i < lab.getX(); i++){
@@ -85,77 +70,103 @@ public class Dijkstra {
                     poidLab[i][j] = lab.getX()* lab.getY();
                 }
                 else {
-                    poidLab[i][j] = 0;
+                    poidLab[i][j] = -1;
                 }
             }
         }
         int[] sorti = getSortie();
         poidLab[sorti[0]][sorti[1]] = 0;
-        
 
         return poidLab;
     }
 
-    public void pel(List<int[]> adj, int[] sorti){
-        PriorityQueue<int[]> file = new PriorityQueue<>();
-        PriorityQueue<int[]> marques = new PriorityQueue<>();
+    public int[][] setWeight(int[] sorti, int[][] poids){
+
+        Queue<int[]> file = new ArrayDeque<>();
         file.add(sorti);
-        marques.add(sorti);
-        while (!file.isEmpty()){
-            int[] tab = file.poll();
-            for(int i = 0; i < adj.size(); i++){
-                if(i )
+
+        while(!file.isEmpty()){
+
+            int[] premier = file.poll();
+            int x = premier[0];
+            int y = premier[1];
+
+            int[] dx = {-1, 1, 0, 0};
+            int[] dy = {0, 0, -1, 1};
+
+
+            for(int i = 0 ; i<4 ; i++){
+                int nextX = x + dx[i];
+                int nextY = y + dy[i];
+
+                if(nextX >= 0 && nextX < lab.getX() && nextY >=0 && nextY < lab.getY()){
+                    if(poids[nextX][nextY]== -1){
+                        file.add(new int[]{nextX, nextY});
+                        poids[nextX][nextY] = poids[x][y] + 1;
+                    }
+                }
+            }
+
+        }
+        return poids;
+    }
+    public List<int[]> retrouverChemin(int[][] poids, int[] depart, int[] arriver) {
+        List<int[]> chemin = new ArrayList<>();
+        int[] courante = depart;
+        chemin.add(courante);
+
+        while (!(courante[0]== arriver[0] && courante[1] == arriver[1])) {
+            int x = courante[0];
+            int y = courante[1];
+            int poidsCourant = poids[x][y];
+            int[] prochaineCase = null;
+
+            int[][] voisins = voisins(poids, new int[]{x, y});
+
+            for (int i = 0; i < voisins.length; i++) {
+                int nextX = voisins[i][0];
+                int nextY = voisins[i][1];
+                int voisin = poids[nextX][nextY];
+                int poidsVoisin = poids[nextX][nextY];
+
+                if (poidsVoisin == poidsCourant - 1) {
+                    prochaineCase = new int[]{voisin};
+                    break;
+                }
+            }
+
+            if (prochaineCase != null) {
+                chemin.add(prochaineCase);
+                courante = prochaineCase;
+            } else {
+                break;
             }
         }
+
+        return chemin;
     }
 
-
-
-
-    private boolean isValidCell(int x, int y) {
-        int rows = lab.getX();
-        int cols = lab.getY();
-        return x >= 0 && x < rows && y >= 0 && y < cols && lab.getLesCases()[x][y].isAccessible();
-    }
-
-    public void getListeCoordonnees() {
-        int i;
-        int j;
-        for (i = 0; i < lab.getX(); i++) {
-            List<List<Integer>> liste2 = new ArrayList<>();
-            for (j = 0; j < lab.getY(); j++) {
-                List<Integer> liste1 = new ArrayList<>();
-                liste1.add(i);
-                liste1.add(j);
-                liste2.add(liste1);
-            }
-            listeCoordonnees.add(liste2);
-        }
-        //afficher();
-    }
-
-    public void afficher() {
-        int i;
-        int j;
-        for (i = 0; i < listeCoordonnees.size(); i++) {
-            for (j = 0; j < listeCoordonnees.get(0).size(); j++) {
-                System.out.print(listeCoordonnees.get(i).get(j) + "\t");
-            }
-            System.out.println();
-        }
-    }
 
     public static void main(String[] args) {
         Labyrinthe lab = new Labyrinthe();
         lab.genererGrilleAleatoire();
         lab.getLesCases()[5][0].setSortie(true);
         Dijkstra dijkstra = new Dijkstra(lab, lab.getLesCases()[5][0]);
-        int[][] dj = dijkstra.setWeight();
+        int[][] dj = dijkstra.initPoids();
+        int[][] poids = dijkstra.setWeight(dijkstra.getSortie(), dj );
+
         for(int i = 0; i < lab.getX(); i++) {
             for (int j = 0; j < lab.getY(); j++) {
-                System.out.print(dj[i][j] +"\t");
+                System.out.print(poids[i][j] +"\t");
             }
             System.out.println();
         }
+        //System.out.println(Arrays.deepToString(dijkstra.voisins(poids, new int[]{5, 2})));
+        List<int[]> chemin = dijkstra.retrouverChemin(poids, new int[]{1, 9},  new int[]{5, 0} );
+
+        for(int i = 0 ; i < chemin.size(); i++){
+            System.out.println(Arrays.toString(chemin.get(i)));
+        }
     }
+
 }
