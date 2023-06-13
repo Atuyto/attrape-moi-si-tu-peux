124b980a5256a717d78a7c86627d6c95350c1c04
Atuyto <111154530+Atuyto@users.noreply.github.com>
2023-06-13 10:14:56 +0200

    A* FINIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

--- attrape_moi_si_tu_peux/src/main/java/com/example/attrape_moi_si_tu_peux/Model/Astar.java
+++ attrape_moi_si_tu_peux/src/main/java/com/example/attrape_moi_si_tu_peux/Model/Astar.java
@@ -1,55 +1,162 @@
 package com.example.attrape_moi_si_tu_peux.Model;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.example.attrape_moi_si_tu_peux.Model.Labyrinthe;
+
+import java.util.*;
 
 public class Astar {
+    private Labyrinthe lab;
+    private int[] depart;
+    private int[] arrivee;
+    private int[][] poids;
 
-    private Labyrinthe lelaby;
-
-    private List<List<List<Integer>>> listeCoordonnees;
-
-    private Case depart;
-
-    private Case arrivee;
-
-
-    public Astar(Labyrinthe labyrinthe,Case depart,Case arrivee) {
-        this.lelaby = labyrinthe;
-        this.listeCoordonnees = new ArrayList<List<List<Integer>>>();
-        this.depart = depart ;
+    public Astar(Labyrinthe labyrinthe, int[] depart, int[] arrivee) {
+        this.lab = labyrinthe;
+        this.depart = depart;
         this.arrivee = arrivee;
+        this.poids = initPoids();
     }
 
-    public void getHeuritisque(Case depart, Case arrivee) {
+    private int[][] initPoids() {
+        int[][] poidsLab = new int[lab.getX()][lab.getY()];
 
-    }
-
-    public void resolutionAstar() {
-
-    }
-
-    public void getListeCoordonnees() {
-        int i;
-        int j;
-        for (i = 0; i < this.lelaby.getX(); i++) {
-            List<List<Integer>> liste2 = new ArrayList<>();
-            for (j = 0; j < this.lelaby.getY(); j++) {
-                List<Integer> liste1 = new ArrayList<>();
-                liste1.add(i);
-                liste1.add(j);
-                liste2.add(liste1);
+        for (int i = 0; i < lab.getX(); i++) {
+            for (int j = 0; j < lab.getY(); j++) {
+                if (lab.getLesCases()[i][j].getContenu() instanceof Rocher) {
+                    poidsLab[i][j] = lab.getX() * lab.getY();
+                } else {
+                    poidsLab[i][j] = -1;
+                }
             }
-            listeCoordonnees.add(liste2);
         }
-        System.out.println(listeCoordonnees);
+
+        poidsLab[arrivee[0]][arrivee[1]] = 0;
+        return poidsLab;
     }
 
+    public int[][] setWeight(int[] sorti, int[][] poids){
+
+        Queue<int[]> file = new ArrayDeque<>();
+        file.add(sorti);
+
+        while(!file.isEmpty()){
+
+            int[] premier = file.poll();
+            int x = premier[0];
+            int y = premier[1];
+
+            int[] dx = {-1, 1, 0, 0};
+            int[] dy = {0, 0, -1, 1};
+
+
+            for(int i = 0 ; i<4 ; i++){
+                int nextX = x + dx[i];
+                int nextY = y + dy[i];
+
+                if(nextX >= 0 && nextX < lab.getX() && nextY >=0 && nextY < lab.getY()){
+                    if(poids[nextX][nextY]== -1){
+                        file.add(new int[]{nextX, nextY});
+                        poids[nextX][nextY] = poids[x][y] + 1;
+                    }
+                }
+            }
+
+        }
+        return poids;
+    }
+
+    private int estimerDistance(int[] coord) {
+        return Math.abs(coord[0] - arrivee[0]) + Math.abs(coord[1] - arrivee[1]);
+    }
+
+
+    public List<int[]> retrouverChemin(int[][] poids, int[] coordDepart, int[] coordArrivee) {
+        List<int[]> chemin = new ArrayList<>();
+        int[] courante = coordDepart;
+        chemin.add(courante);
+
+        while (!(courante[0] == coordArrivee[0] && courante[1] == coordArrivee[1])) {
+            int x = courante[0];
+            int y = courante[1];
+            int poidsCourant = poids[x][y];
+            int[] prochaineCase = null;
+
+            int[] dx = {-1, 1, 0, 0};
+            int[] dy = {0, 0, -1, 1};
+
+            int distanceMin = Integer.MAX_VALUE;
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = x + dx[i];
+                int nextY = y + dy[i];
+
+                if (nextX >= 0 && nextX < poids.length && nextY >= 0 && nextY < poids[0].length) {
+                    int poidsVoisin = poids[nextX][nextY];
+
+                    if (poidsVoisin == poidsCourant - 1) {
+                        int distanceEstimee = Math.abs(nextX - coordArrivee[0]) + Math.abs(nextY - coordArrivee[1]);
+
+                        if (distanceEstimee < distanceMin) {
+                            distanceMin = distanceEstimee;
+                            prochaineCase = new int[]{nextX, nextY};
+                        }
+                    }
+                }
+            }
+
+            if (prochaineCase != null) {
+                chemin.add(prochaineCase);
+                courante = prochaineCase;
+            } else {
+                break;
+            }
+        }
+
+        return chemin;
+    }
+
+
+
+    public int[] getSortie(){
+        int x = 0;
+        int y = 0;
+        for(int i = 0 ; i< lab.getX() ; i++){
+            for (int j = 0; j<lab.getY(); j++){
+                if(this.lab.getLesCases()[i][j].getSortie()){
+                    x= i;
+                    y =j;
+                    break;
+                }
+            }
+        }
+        return new int[]{x,y};
+    }
 
     public static void main(String[] args) {
         Labyrinthe lab = new Labyrinthe();
-        Astar aStar;
-        aStar = new Astar(lab,lab.getLesCases()[0][0],lab.getLesCases()[9][9]);
-        aStar.getListeCoordonnees();
+        lab.genererGrilleAleatoire();
+        lab.getLesCases()[5][0].setSortie(true);
+
+        int[] depart = {1, 8};
+        int[] arrivee = {5, 0};
+
+
+
+
+        Astar astar = new Astar(lab, depart, arrivee);
+        int[][] dj = astar.initPoids();
+        int[][] poids = astar.setWeight(astar.getSortie(), dj );
+
+        for(int i = 0; i < lab.getX(); i++) {
+            for (int j = 0; j < lab.getY(); j++) {
+                System.out.print(poids[i][j] +"\t");
+            }
+            System.out.println();
+        }
+        List<int[]> chemin = astar.retrouverChemin(poids, depart, arrivee);
+
+        for (int[] coord : chemin) {
+            System.out.println(Arrays.toString(coord));
+        }
     }
 }
